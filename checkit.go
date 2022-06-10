e586d6bb5d89f458bce8772b976677530639b75d
Moser Michael <moser.michael@gmail.com>
2022-06-10 06:17:00 +0300

    update

--- checkit.go
+++ checkit.go
@@ -22,6 +22,24 @@ func passStringStruct(m StrStruct) {
 	m.firstVal = "changed_val"
 }
 
+/*
+The actual output:
+
+function parameters that are maps are passed by reference
+function parameters that are arrays are passed by reference
+function parameters that are structs are passed by value !!!
+
+map lookup of sequence, the value is returned by reference!
+you got a string from map to scalar lookup
+map lookup of scalar, the value is returned by value
+map lookup of struct, the value is returned by value
+
+********************************
+now you learned something
+(yeah, and simple go is confusing the heck out of me)
+(why are there always such inconsistencies???)
+********************************
+*/
 func main() {
 
 	mapArg := map[string]string{
@@ -31,7 +49,7 @@ func main() {
 	passStringMap(mapArg)
 
 	if mapArg["first_key"] != "first_key" {
-		fmt.Print("maps are passed by reference\n")
+		fmt.Print("function parameters that are maps are passed by reference\n")
 	} else {
 		panic("You thought maps are passed by value? wrong!")
 	}
@@ -41,7 +59,7 @@ func main() {
 	passStringVec(vecStr)
 
 	if vecStr[0] != "first" {
-		fmt.Printf("arrays are passe by reference\n")
+		fmt.Printf("function parameters that are arrays are passed by reference\n")
 	} else {
 		panic("You thought arrays are passed by value? wrong!")
 	}
@@ -51,12 +69,51 @@ func main() {
 	passStringStruct(structStr)
 
 	if structStr.firstVal == "first" {
-		fmt.Printf("Structs are passed by value\n")
+		fmt.Printf("function parameters that are structs are passed by value !!!\n\n")
 	} else {
 		panic("You thought structs are passed by reference? Wrong, this is not java!")
 	}
 
-	fmt.Printf(`********************************
+	mapToVector := map[string]StrVec{
+		"first": []string{"one", "two", "three"},
+	}
+
+	val, _ := mapToVector["first"]
+
+	val[0] = "changed_it"
+
+	if mapToVector["first"][0] != "one" {
+		fmt.Printf("map lookup of sequence, the value is returned by reference!\n")
+	} else {
+		panic("You were wrong, the lookup returned a reference value!\n")
+	}
+
+	mapToString := map[string]string{"one": "first"}
+	strVal, _ := mapToString["one"]
+	fmt.Printf("you got a %T from map to scalar lookup\n", strVal)
+	strVal = "actually-this-value-is-not-used"
+
+	if mapToString["one"] == "first" {
+		fmt.Printf("map lookup of scalar, the value is returned by value\n")
+	} else {
+		panic("You were wrong. map lookup with scalar values does not return a reference\n")
+	}
+
+	mapStrToStruct := map[string]StrStruct{
+		"one": StrStruct{firstVal: "first"},
+	}
+
+	stuctVal, _ := mapStrToStruct["one"]
+	stuctVal.firstVal = "changed_it?"
+
+	if mapStrToStruct["one"].firstVal == "first" {
+		fmt.Printf("map lookup of struct, the value is returned by value\n")
+	} else {
+		panic("You were wrong. map lookup of struct value does not return a reference value\n")
+	}
+
+	fmt.Printf(`
+********************************
 now you learned something
 (yeah, and simple go is confusing the heck out of me)
 (why are there always such inconsistencies???)
--- dev-gotchas.md
+++ dev-gotchas.md
@@ -267,9 +267,9 @@ Remembering the details of golang, i wasn't exposed to this language for quite s
   the struct that was within the array...
   (actually I nevere liked C++ references, almost never used them, unless forced at gunpoint)
 
-  To add insult to injury: Maps are always passed by reference! They did that, because the authors of the language realized, that nobody is getting the difference.... [link](https://stackoverflow.com/questions/40680981/are-maps-passed-by-value-or-by-reference-in-go)
-  Yes, and vectors are also passed by reference, but structs are passed by value! And the 'tour of go' doesn't tell you...
-  Don't believe it? Look [here](checkit.go)
+  To add insult to injury: Maps are always passed by reference! They did that, because the authors of the language realized, that nobody is getting the difference.... [link](https://stackoverflow.com/questions/40680981/are-maps-passed-by-value-or-by-reference-in-go) Yes, and vectors are also passed by reference, but structs are passed by value! And the 'tour of go' doesn't tell you... Don't believe it? Look [here](checkit.go)
+
+  The same goes for contents of maps (an vectors), If you have a nested collection as the value for a map, then map lookup returns a reference value, otherwise you get a map lookup by value...
 
 - the following signatures has a problem: what do you do about returning errors?
   Here you would need to return some value of SomeType togather with the error.
