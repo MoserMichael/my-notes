f07e7bbcd91162ce7786a04de991e5e47fa4bc1f
MoserMichael <moser.michael@gmail.com>
2023-11-09 09:56:40 +0200

    leetcode

---
+++ interview-questions/dynamic-programming/leetcode-53-maximum-subsum.py
@@ -0,0 +1,61 @@
+# 53. Maximum Subarray
+# Medium
+#    Given an integer array nums, find the
+#    subarray
+#    with the largest sum, and return its sum.
+#
+#     
+#
+#    Example 1:
+#
+#    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+#    Output: 6
+#    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+#
+#    Example 2:
+#
+#    Input: nums = [1]
+#    Output: 1
+#    Explanation: The subarray [1] has the largest sum 1.
+#
+#    Example 3:
+#
+#    Input: nums = [5,4,-1,7,8]
+#    Output: 23
+#    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+#
+#     
+#
+#    Constraints:
+#
+#        1 <= nums.length <= 105
+#        -104 <= nums[i] <= 104
+#
+#     
+#
+#    Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+#
+
+import math
+
+class Solution:
+
+    # that's cadanes algorithm. Isn't it?
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        if len(nums) == 0:
+            return 0
+
+        current_sum = nums[0]
+        abs_sum = nums[0]
+        for idx in range(1, len(nums)):
+
+            s = current_sum + nums[idx]
+            if s <  nums[idx]:
+                current_sum = nums[idx]
+            else:
+                current_sum = s
+
+            abs_sum = max(abs_sum, current_sum)
+
+        return abs_sum
