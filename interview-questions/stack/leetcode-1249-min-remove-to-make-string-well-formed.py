c35e9134d876679eb2a333689dfdf91ccba1adba
MoserMichael <mosermichael@gmail.com>
2024-04-06 11:27:52 +0200

    upd

--- interview-questions/stack/leetcode-1249-min-remove-to-make-string-well-formed.py
+++ interview-questions/stack/leetcode-1249-min-remove-to-make-string-well-formed.py
@@ -38,8 +38,26 @@
 #        s[i] is either'(' , ')', or lowercase English letter.
 #
 
+# Intuition
+# Code
 
-class Solution:
+
+### submission ####
+#
+#    keep a stack to determine the well formedness of the parenthesis set, the stack entries are the index of the parenthesis in the string.
+#    - a ( sign adds a new entry to the stack.
+#    - a ) sign:
+#       - if the stack is non empty then pop the top of the stack, the current ) sign is part of a a well formed expression.
+#       -  if the stack is empty then this is a not well formed expression. Add the positon of the ) sign to the positions-of-offending-parenthesis array
+#
+#
+#    at the end of the loop: all remaining parenthesis postions on the stack to the positions-of-offending-parenthesis array. (note: this array is now sorted in ascending order)
+#
+#    Second loop: use io.StringIO to gather all the text in between the positions given by the array of offending parenthesis. (using io.StringIO is much faster then string manipulation)
+#
+
+
+lass Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stk = []
         remove_idx = []
@@ -58,9 +76,20 @@ class Solution:
 
         #print(remove_idx)
 
-        ret = s
-        for idx in remove_idx[::-1]:
-            ret = ret[0:idx] + ret[(idx+1):]
-            #print(idx, ret)
+        ret = io.StringIO()
 
-        return ret
+        prev_idx = 0
+        for idx in remove_idx:
+            ret.write(s[prev_idx:idx])
+            prev_idx = idx+1
+        ret.write(s[prev_idx:])
+
+        return ret.getvalue()
+
+        #ret = s
+        #for idx in remove_idx[::-1]:
+        #    ret = ret[0:idx] + ret[(idx+1):]
+        #    #print(idx, ret)
+        # return ret
+
+
